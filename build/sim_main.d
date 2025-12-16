379e919775796698adf5da8a7121fd544d92dbb6
Chris Jimenez <ChrisJimenez0818@gmail.com>
2025-12-16 03:39:50 -0600

    Fixed rars path and removed unnecessary file

--- build/sim_main.d
+++
@@ -1,9 +0,0 @@
-sim_main.o: ../sim/sim_main.cpp \
- /opt/oss-cad-suite/share/verilator/include/verilated.h \
- /opt/oss-cad-suite/share/verilator/include/verilatedos.h \
- /opt/oss-cad-suite/share/verilator/include/verilated_config.h \
- /opt/oss-cad-suite/share/verilator/include/verilated_types.h \
- /opt/oss-cad-suite/share/verilator/include/verilated_funcs.h \
- /opt/oss-cad-suite/share/verilator/include/verilated_vcd_c.h \
- /opt/oss-cad-suite/share/verilator/include/verilated.h \
- /opt/oss-cad-suite/share/verilator/include/verilated_trace.h Vrisc_v.h
